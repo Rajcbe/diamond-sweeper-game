5340a630a16a43977c20ef9830aadb55fb40df51
Pushparaj <pushparaj@graspio.com>
2018-06-02 19:46:31 +0530

    Mobile version address bar meta color added.

--- public/index.bundle.js
+++ public/index.bundle.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "89f7af327fd6cc88b44b"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "52734c7167d0b920d173"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
